34f4024f7d83534c731c3cf8a6c23be006bf358e
siravit <siravit.ngamjee@gmail.com>
2020-08-25 16:12:42 +0700

    add sdk connect wifi for esp8266

--- iFraESP8266SDK.cpp
+++ iFraESP8266SDK.cpp
@@ -1,6 +1,20 @@
 #include "iFraESP8266SDK.h"
 
-void iFraESP8266SDK::Connect() {
-   Serial.print("Hello iFraESP8266SDK");
+iFraESP8266SDK::iFraESP8266SDK(char* ssid, char* password){
+    this->ssid_=ssid;
+    this->password_=password;
 }
- 
+
+void iFraESP8266SDK::Connect() {
+      WiFi.begin(this->ssid_, this->password_);
+      Serial.print("Start connect wifi");
+      while (WiFi.status() != WL_CONNECTED) 
+         {
+            delay(500);
+            Serial.print(".");
+         }
+      WiFi.setAutoReconnect(true);
+      Serial.println("WiFi connected");  
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+}
--- iFraESP8266SDK.h
+++ iFraESP8266SDK.h
@@ -6,11 +6,17 @@
 
 #endif // HARDWAREPLATFORM_H
 #include <PubSubClient.h>
+#include <ESP8266WiFi.h>
 
 class iFraESP8266SDK : public HardWarePlatform
 {
    public:
+        char* ssid_;
+        char* password_;
+        WiFiClient client_;
+        iFraESP8266SDK(char* ssid, char* password);
         void Connect() override;
+        Client* GetClient() override;
 };
 
 
